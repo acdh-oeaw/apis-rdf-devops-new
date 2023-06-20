118490a6872b0a7138e06dc40bd10d1759d61d4f
Birger Schacht <birger.schacht@oeaw.ac.at>
2023-06-20 11:20:34 +0200

    move to prosnet workflow

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [[ "${APIS_RDF_ONTOLOGY}" ]]; then
-	ln -s ${APIS_RDF_ONTOLOGY} apis_ontology
-	pip install -r ${APIS_RDF_ONTOLOGY}/requirements.txt
-fi
-gunicorn wsgi -b 0.0.0.0:5000 --timeout 120 --workers=3 --threads=3 --worker-connections=1000
